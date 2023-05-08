7300b1588842b5c748fea27e82eba90a29fd51c7
Siddhant Sanjay Agarwal <98652493+fLeXnotOP@users.noreply.github.com>
2023-05-08 23:18:30 +0530

    Time: 301 ms (11.85%), Space: 8.1 MB (84.05%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -2,36 +2,39 @@ class Solution {
 public:
     
     // dp vector contains minimum of maximum guess losses in range L and H
-    int dp[201][201];
     
     // returns minimum of maximum guess losses in range L and H
-    int getMinimum(int l, int h)
+    int getMinimum(int start, int end,vector<vector<int>>& dp)
     {
         // base case: L>=H then there is no way to guess wrong and so loss is 0
-        if(h<=l) return 0;
+        if(start>=end){
+            return 0;
+        }
         
         // return precalculated values
-        if(dp[l][h]!=-1) return dp[l][h];
+        if(dp[start][end]!=-1){
+            return dp[start][end];
+        }
         
         // min of max guess loss 
         int res = INT_MAX;
         
         // guess value from L to H
-        for(int i=l;i<=h;i++)
+        for(int i=start;i<=end;i++)
         {
             // guess number i and it is wrong, so the possible losses are
             // i+getMinimum(l,i-1) and i+getMinimum(i+1,h) and we take the max loss
             // get minimum loss between res and max guess loss
-            res = min(res, i + max(getMinimum(l,i-1),getMinimum(i+1,h)));
+            res = min(res, i + max(getMinimum(start,i-1,dp),getMinimum(i+1,end,dp)));
         }
         
-        return dp[l][h] = res;
+        return dp[start][end] = res;
     }
     
     int getMoneyAmount(int n){
         
-        memset(dp,-1,sizeof(dp));
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
         
-        return getMinimum(1,n);
+        return getMinimum(1,n,dp);
     }
 };
