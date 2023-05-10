e9a4f13b7aa5dba47274584f3b4222bd06806789
Siddhant Sanjay Agarwal <98652493+fLeXnotOP@users.noreply.github.com>
2023-05-11 00:56:53 +0530

    Time: 16 ms (15.45%), Space: 15.2 MB (29.68%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -25,11 +25,40 @@ public:
             profit = max(sell,skip);
         } 
         return dp[index][buy] = profit;
+    } 
+    
+    int solveTab(vector<int>& prices){
+        int n = prices.size(); 
+        vector<vector<int>> dp(n+1,vector<int>(2,0)); 
+        
+        
+        for(int index = n-1;index>=0;index--){
+            for(int buy=0;buy<=1;buy++){
+                
+                int profit = 0; 
+                
+                if(buy==1){
+                    int buy = -prices[index] + dp[index+1][0]; 
+                    int skip = 0 + dp[index+1][1]; 
+                    profit = max(buy,skip);
+                } 
+                else{
+                    int sell = prices[index] + dp[index+1][1]; 
+                    int skip = 0 + dp[index+1][0]; 
+                    profit = max(sell,skip);
+                } 
+                
+                dp[index][buy] = profit;
+            }
+        } 
+        
+        return dp[0][1];
+        
     }
     
     int maxProfit(vector<int>& prices) {  
-        int n = prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return solve(0,1,prices,dp);
+        //int n = prices.size();
+        //vector<vector<int>> dp(n,vector<int>(2,-1));
+        return solveTab(prices);
     }
 };
