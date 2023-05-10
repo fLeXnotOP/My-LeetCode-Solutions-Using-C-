e390683d2cb8a5f9c67ad85e65938080e9abeae5
Siddhant Sanjay Agarwal <98652493+fLeXnotOP@users.noreply.github.com>
2023-05-11 01:02:06 +0530

    Time: 16 ms (15.45%), Space: 13.1 MB (64.61%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -29,7 +29,9 @@ public:
     
     int solveTab(vector<int>& prices){
         int n = prices.size(); 
-        vector<vector<int>> dp(n+1,vector<int>(2,0)); 
+        
+        vector<int> curr(2,0); 
+        vector<int> next(2,0);
         
         
         for(int index = n-1;index>=0;index--){
@@ -38,21 +40,22 @@ public:
                 int profit = 0; 
                 
                 if(buy==1){
-                    int buy = -prices[index] + dp[index+1][0]; 
-                    int skip = 0 + dp[index+1][1]; 
+                    int buy = -prices[index] + next[0]; 
+                    int skip = 0 + next[1]; 
                     profit = max(buy,skip);
                 } 
                 else{
-                    int sell = prices[index] + dp[index+1][1]; 
-                    int skip = 0 + dp[index+1][0]; 
+                    int sell = prices[index] + next[1]; 
+                    int skip = 0 + next[0]; 
                     profit = max(sell,skip);
                 } 
                 
-                dp[index][buy] = profit;
-            }
+                curr[buy] = profit;
+            } 
+            next=curr;
         } 
         
-        return dp[0][1];
+        return next[1];
         
     }
     
