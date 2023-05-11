aaf2060e80f9b5258837f2a02b7e91c28cdd4f77
Siddhant Sanjay Agarwal <98652493+fLeXnotOP@users.noreply.github.com>
2023-05-11 11:20:04 +0530

    Time: 19 ms (67.78%), Space: 13 MB (63.96%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -17,17 +17,29 @@ public:
         dp[index1][index2]=max(solveMem(index1+1,index2,nums1,nums2,dp),solveMem(index1,index2+1,nums1,nums2,dp)); 
         
         return dp[index1][index2];
+    }  
+
+//Bottom-Up dp 
+    int solveTab(vector<int>& A, vector<int>& B){ 
+        int n = A.size(); 
+        int m = B.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++)
+            for(int j=1;j<=m;j++)
+                A[i-1]==B[j-1]?dp[i][j] = max( dp[i][j], dp[i-1][j-1] + 1 ):dp[i][j]= max(dp[i][j-1],dp[i-1][j]);
+        return dp[n][m];
     }
     
     
     
     
     
+    
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) { 
-        int n = nums1.size(); 
-        int m = nums2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1)); 
+        //int n = nums1.size(); 
+        //int m = nums2.size();
+        //vector<vector<int>> dp(n+1,vector<int>(m+1,-1)); 
         
-        return solveMem(0,0,nums1,nums2,dp);
+        return solveTab(nums1,nums2);
     }
 };
