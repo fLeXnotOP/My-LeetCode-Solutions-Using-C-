be41d867d4d7a37624ad1f9a17aa6bf826348deb
Siddhant Sanjay Agarwal <98652493+fLeXnotOP@users.noreply.github.com>
2023-05-18 00:13:59 +0530

    Time: 75 ms (18.61%), Space: 19.1 MB (20.99%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,81 @@
+class Solution {  
+        vector<int> nextSmallerElement(vector<int> arr,int n){
+        stack<int> s; 
+        
+        s.push(-1);  
+        
+        vector<int> ans(n);
+        
+        for(int i= n-1;i>=0;i--){
+            int curr=arr[i]; 
+            while(s.top()!=-1 && arr[s.top()]>=curr){
+                s.pop();
+            } 
+            ans[i]=s.top(); 
+            s.push(i);
+        } 
+        return ans;
+    } 
+    
+    vector<int> prevSmallerElement(vector<int> arr,int n){
+        stack<int> s; 
+        s.push(-1); 
+        vector<int> ans(n); 
+        
+        for(int i=0;i<n;i++){
+            int curr=arr[i]; 
+            while(s.top()!=-1 && arr[s.top()]>=curr){
+                s.pop();
+            } 
+            ans[i]=s.top(); 
+            s.push(i);
+        } 
+        return ans;
+    }
+
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size(); 
+        
+        vector<int> next(n);
+        next = nextSmallerElement(heights,n); 
+        
+        vector<int> prev(n); 
+        prev = prevSmallerElement(heights,n); 
+        int area=0;
+        for(int i=0;i<n;i++){
+            int length = heights[i]; 
+            
+            if(next[i]==-1){
+                next[i]=n;
+            } 
+            int breadth = next[i] - prev[i] - 1; 
+            
+            int newArea = length*breadth; 
+            
+            area=max(area,newArea);
+        } 
+        
+        return area;
+    }
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) { 
+        int maxi = INT_MIN; 
+        
+        vector<int> histogram(matrix[0].size(),0); 
+        
+        for(int i=0;i<matrix.size();i++){
+            //for creating histtogram array  
+            for(int j=0;j<matrix[0].size();j++){
+                if(matrix[i][j]=='1'){
+                    histogram[j]++;
+                } 
+                else{
+                    histogram[j]=0;
+                } 
+                
+            } 
+            maxi=max(maxi,largestRectangleArea(histogram));
+        } 
+        return maxi;
+    }
+};
