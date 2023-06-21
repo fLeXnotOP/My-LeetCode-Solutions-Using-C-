998a121fc83144a058a32e2d2abc476fe552ef53
Siddhant Sanjay Agarwal <98652493+fLeXnotOP@users.noreply.github.com>
2023-06-21 11:25:31 +0530

    Time: 101 ms (73.36%), Space: 38.4 MB (94.89%) - LeetHub

---
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    
+        long long minCost(vector<int>& n, vector<int>& cost) {
+    long long l = 1, r = 1000000, res = 0, res1 = 0;
+    while (l < r) {
+        int m = (l + r) / 2;
+        res = res1 = 0;
+        for (int i = 0; i < n.size(); ++i) {
+            res += (long long)cost[i] * abs(n[i] - m);
+            res1 += (long long)cost[i] * abs(n[i] - (m + 1));
+        }
+        if (res < res1)
+            r = m;
+        else
+            l = m + 1;
+    }
+    return min(res, res1);    
+}
+    
+};
