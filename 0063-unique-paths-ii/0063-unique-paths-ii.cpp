74a64bbec1404eedc91c6ae409b71947c7c1a856
Siddhant Sanjay Agarwal <98652493+fLeXnotOP@users.noreply.github.com>
2023-10-23 17:09:18 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (6.96%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -8,7 +8,28 @@ public:
         int left=solveMemo(i,j-1,grid,dp);
         
         return dp[i][j]=up+left;
+    } 
+    
+    int solveTabu(int m,int n,vector<vector<int>>&grid){
+        vector<vector<int>>dp(m,vector<int>(n,0));
+         for(int i=0;i<m;i++){
+             for(int j=0;j<n;j++){
+                 if(grid[i][j]==1) continue;
+                 if(i==0 && j==0 ) dp[i][j]=1;
+                 else{
+                     int up=0,left=0;
+                     if(i>0) up=dp[i-1][j];
+                     if(j>0) left=dp[i][j-1];
+                     
+                     dp[i][j]=up+left;
+                 }
+             }
+         }
+         return dp[m-1][n-1];
+
     }
+    
+
     
     
     
@@ -17,6 +38,6 @@ public:
         int n = obstacleGrid[0].size();
         vector<vector<int>> dp(m+1,vector<int>(n+1,-1)); 
         
-        return solveMemo(m-1,n-1,obstacleGrid,dp);
+        return solveTabu(m,n,obstacleGrid);
     }
 };
