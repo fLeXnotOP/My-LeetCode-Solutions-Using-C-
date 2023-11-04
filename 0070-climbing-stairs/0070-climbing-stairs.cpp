291d6c490d9378b0109995e3b1f46d080e47e090
Siddhant Sanjay Agarwal <98652493+fLeXnotOP@users.noreply.github.com>
2023-11-04 22:51:26 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (26.71%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public: 
+    int solveMem(int n,vector<int>& dp){
+        if(n==1){
+            return 1;
+        } 
+        if(n==2){
+            return 2;
+        } 
+        
+        if(dp[n]!=-1){
+            return dp[n];
+        } 
+        
+        int ans = solveMem(n-1,dp) + solveMem(n-2,dp); 
+        
+        dp[n] = ans; 
+        
+        return dp[n];
+    }
+    
+    int climbStairs(int n) {  
+        vector<int> dp(n+1,-1); 
+        return solveMem(n,dp);
+
+        
+
+        
+    }
+};
